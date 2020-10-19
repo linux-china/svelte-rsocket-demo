2242f59028be6803d7cd499565f7abc68802f023
linux_china <libing.chen@gmail.com>
2020-10-18 17:21:40 -0700

    App.svelte

---
+++ src/App.svelte
@@ -0,0 +1,24 @@
+<script>
+    export let name;
+    let rsocketRequestCounter = 0;
+    let response = "";
+    import {rsocket} from "./rsocket-client"
+
+    async function handlerClick() {
+        response = await rsocket.requestResponse("demo.service", "HI")
+        rsocketRequestCounter = rsocketRequestCounter + 1;
+    }
+</script>
+
+<style>
+    h1 {
+        color: purple;
+    }
+</style>
+
+<h1>Hello {name}!</h1>
+
+
+<button on:click={handlerClick}>RSocket RPC</button>
+
+<h3>Response({rsocketRequestCounter}): {response}</h3>

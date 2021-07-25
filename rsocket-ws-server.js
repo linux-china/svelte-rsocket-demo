0aa17e79702680b8b8d3950a6e4789d1e2417d5c
linux_china <libing.chen@gmail.com>
2021-07-25 16:41:26 -0700

    Polishing

--- rsocket-ws-server.js
+++ rsocket-ws-server.js
@@ -1,44 +1,33 @@
 'use strict';
 
-const Deferred = require('fbjs/lib/Deferred');
 const {Single} = require('rsocket-flowable');
 const {RSocketServer} = require('rsocket-core');
-const RSocketWebSocketServer = require('rsocket-websocket-server').default;
+const RSocketWebSocketServer = require('rsocket-websocket-server');
 
-async function receive() {
-    const server = new RSocketServer({
-        getRequestHandler(socket, payload) {
-            return {
-                requestResponse: (payload) => {
-                    console.log('request: ' + payload.data.toString());
-                    return Single.of({
-                        data: "Pong",
-                        metadata: null
-                    })
+//=== rsocket handler
+const requestHandler = (requestingRSocket, setupPayload) => {
+    return {
+        requestResponse: (payload) => {
+            return new Single((subscriber) => {
+                subscriber.onSubscribe(() => {
+                });
+                console.log('request: ' + payload.data.toString());
+                try {
+                    subscriber.onComplete({
+                        data: 'Pong',
+                        metadata: ''
+                    });
+                } catch (e) {
+                    subscriber.onError(e);
                 }
-            };
-        },
-        transport: new RSocketWebSocketServer({
-            host: '127.0.0.1',
-            port: 8080,
-            path: "/rsocket"
-        }),
-    });
-    return server.start();
-}
+            });
+        }
+    };
+};
 
-async function run() {
-    const rsocket = await receive();
-    console.log("RSocket server started on ws://localhost:8080/rsocket")
-    const deferred = new Deferred();
-    return deferred.getPromise();
-}
+const WebSocketTransport = RSocketWebSocketServer.default;
+const transport = new WebSocketTransport({host: "127.0.0.1", port: 8080, path:"/rsocket"});
+const rsocketServer = new RSocketServer({transport: transport, getRequestHandler: requestHandler});
+rsocketServer.start();
 
-Promise.resolve(run()).then(
-    () => process.exit(0),
-    error => {
-        console.error(error.stack);
-        process.exit(1);
-    },
-);
 
